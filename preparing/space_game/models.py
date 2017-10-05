86268667221d95ddb0c1331b5e539a6964184573
SupasanKomonlit <supasan_224@hotmail.com>
2017-10-05 11:09:02 +0700

    Understand about how to send variable

---
+++ preparing/space_game/models.py
@@ -0,0 +1,20 @@
+class Ship:
+    def __init__(self,world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+    def update(self, delta):
+        if self.y > self.world.height:
+            self.y = 0
+        self.y += delta
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.ship = Ship(self,100,100)
+
+    def update(self, delta):
+        self.ship.update(delta)
--- preparing/space_game/space.py
+++ preparing/space_game/space.py
@@ -1,26 +1,39 @@
 import arcade
 
+from models import Ship, World
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.WHITE)
-        self.ship = arcade.Sprite('images/ship.png')
-        self.ship.set_position(100,100)
+        
+        self.world = World(width, height)
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship) 
 
     def on_draw(self):
         arcade.start_render()
-        self.ship.draw()
+        self.ship_sprite.draw()
 
     def update(self, delta):
-        ship = self.ship
-
-        if ship.center_y > SCREEN_HEIGHT:
-            ship.center_y = 0
-        ship.set_position(ship.center_x, ship.center_y + 5)
+        self.world.update(6)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
