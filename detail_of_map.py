c5f527440911d4f4ca1ebcfa547587967a646658
SupasanKomonlit <supasan_224@hotmail.com>
2017-10-05 21:17:05 +0700

    Edit code about drawing Table for useful about collection variable

--- detail_of_map.py
+++ detail_of_map.py
@@ -1,27 +1,22 @@
 import arcade
 
 class Map:
-    def __init__(self, width_screen, height_screen, width_grid, height_grid):
-        self.width = width_screen
-        self.height = height_screen
-        self.width_grid = width_grid
-        self.height_grid = height_grid
-        self.grid = []
-        for row in range(20):
-            self.grid.append([])
-            for column in range(12):
-                self.grid[row].append(0)
+    def __init__(self, SCREEN_WIDTH, SCREEN_HIGHT, WIDTH, HIGHT, array_map ):
+        self.plan_map = array_map
+        self.width = WIDTH
+        self.hight = HIGHT
+        self.widths = SCREEN_WIDTH
+        self.hights = SCREEN_HIGHT
+        print("len(self.plan_map) is %i"%(len(self.plan_map)))
+        print("len(self.plan_map[0]) is %i"%(len(self.plan_map[0])))
 
 #    def update(self, delta):
 
     def draw(self):
-        for row in range(12):
-            for column in range(20):
-                x = column * (self.width_grid ) + self.width_grid//2
-                y = row * (self.height_grid ) + self.height_grid//2
-
-                arcade.draw_rectangle_filled(x, y,self.width_grid, self.height_grid, arcade.color.BLACK)
-
+        for row in range(len(self.plan_map)):
+            for column in range(len(self.plan_map[row])):
+                arcade.draw_rectangle_filled(column*self.width+self.width/2,row*self.hight+self.hight/2,self.width-1,self.hight-1,arcade.color.BLACK)
+                
 #    def on_key_press(self, key, key_modifiers):
 #        if key == arcade.key.SPACE:
 #            self.ship.switch_direction()
--- run_out_of_zombie.py
+++ run_out_of_zombie.py
@@ -2,22 +2,35 @@ import arcade
 
 from detail_of_map import Map
 
-screen_width = 1000
-screen_height = 600
+NUM_ROW = 12
+NUM_COLUMN = 16
 
-width_of_grid = 50
-height_of_grid = 50
+WIDTH = 51
+HIGHT = 51
+
+SCREEN_WIDTH = NUM_COLUMN * WIDTH
+SCREEN_HIGHT = NUM_ROW * HIGHT
+
 
 class Game_Window(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.WHITE)
-        self.map = Map(width, height, width_of_grid, height_of_grid)
+
+        self.setup_map= []
+        for row in range(NUM_ROW):
+            self.setup_map.append([])
+            for column in range(NUM_COLUMN):
+                self.setup_map[row].append(0)
+        print(self.setup_map)
+
+        self.map = Map(SCREEN_WIDTH,SCREEN_HIGHT,WIDTH,HIGHT,self.setup_map)
+
 
     def on_draw(self):
         arcade.start_render()
         self.map.draw()
 
 if __name__ == '__main__':
-    window = Game_Window(screen_width, screen_height)
+    window = Game_Window(SCREEN_WIDTH, SCREEN_HIGHT)
     arcade.run()
