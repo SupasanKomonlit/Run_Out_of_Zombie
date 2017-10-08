2365877ab79f0062d6e465f1db10b032c9770239
SupasanKomonlit <supasan_224@hotmail.com>
2017-10-08 23:17:36 +0700

    set num of zombie

--- run_out_of_zombie.py
+++ run_out_of_zombie.py
@@ -19,8 +19,8 @@ else:
 
 NUM_WALL = NUM_ROW*NUM_COLUMN*25//100
 
-#NUM_ZOMBIE = NUM_ROW*NUM_COLUMN*8//100
-NUM_ZOMBIE = 5 
+NUM_ZOMBIE = NUM_ROW*NUM_COLUMN*8//100
+#NUM_ZOMBIE = 5 
 class Game_Character(arcade.Sprite):
     def __init__(self, *location_of_picture, **character):
         self.knight = character.pop('knight', None)
